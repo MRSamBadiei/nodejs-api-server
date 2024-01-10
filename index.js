94a83ae1da1a779bc478d4993a1febdcb4f1db99
Amir <69331074+amirh3sam@users.noreply.github.com>
2024-01-10 12:27:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+//note
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
